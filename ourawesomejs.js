d30368b83646be2b55c81f87d8e08383a4357eab
Brad <bjkapteyn@gmail.com>
2020-04-06 20:32:49 -0400

    added fixed nav bar on scroll

--- ourawesomejs.js
+++ ourawesomejs.js
@@ -8,3 +8,26 @@
 
     console.log("hey there");
 })();
+
+window.onscroll = function() {
+    this.sticky();
+}
+let youDidIt = document.getElementById('youDidIt');
+let navBar = document.getElementById('nav');
+let navBarOffsetTop = navBar.offsetTop;
+
+function sticky() {
+    if(window.pageYOffset >= navBarOffsetTop){
+        youDidIt.style.display = "block";
+        navBar.classList.add("navStick");
+    } else {
+        navBar.classList.remove("navStick");
+    }
+    
+};
+
+
+
+window.onscroll = function() {
+    this.sticky();
+}
