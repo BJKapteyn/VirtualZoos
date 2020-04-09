2af69190147270d72e7402a44ef361a89f0abceb
Brad <bjkapteyn@gmail.com>
2020-04-09 15:03:32 -0400

    added next and previous function to the carousel

--- ourawesomejs.js
+++ ourawesomejs.js
@@ -7,24 +7,96 @@ let buttons = {
 };
 
 let carouselCells = {
-    leftCell: document.getElementById('leftCell'),
-    mainCell: document.getElementById('mainCell'),
-    rightCell: document.getElementById('rightCell')
+    leftCell: document.getElementById('leftPic'),
+    mainCell: document.getElementById('mainPic'),
+    rightCell: document.getElementById('rightPic')
 };
 
+buttons.previousButton.addEventListener('click', function() {
+    let leftCellSrc = carouselCells.leftCell.src;
+    let mainCellSrc = carouselCells.mainCell.src;
+    let rightCellSrc = carouselCells.rightCell.src;
+
+    debugger;
+    let indexOfLeftCell = pictureArray.indexOf(leftCellSrc);
+    let indexOfMainCell = pictureArray.indexOf(mainCellSrc);
+    let indexOfRightCell = pictureArray.indexOf(rightCellSrc);
+
+    if(indexOfLeftCell == 0) {
+        indexOfLeftCell = pictureArray.length - 1;
+        indexOfMainCell--;
+        indexOfRightCell--;
+    } else if(indexOfMainCell == 0) {
+        indexOfLeftCell--;
+        indexOfMainCell = pictureArray.length - 1;
+        indexOfRightCell--;
+    } else if(indexOfRightCell == 0) {
+        indexOfLeftCell--;
+        indexOfMainCell--;
+        indexOfRightCell = pictureArray.length - 1;
+    } else {
+        indexOfLeftCell--;
+        indexOfMainCell--;
+        indexOfRightCell--;
+    }
+
+    carouselCells.leftCell.src = pictureArray[indexOfLeftCell];
+    carouselCells.mainCell.src = pictureArray[indexOfMainCell];
+    carouselCells.rightCell.src = pictureArray[indexOfRightCell];
+});
+
+buttons.nextButton.addEventListener('click', function() {
+    let leftCellSrc = carouselCells.leftCell.src;
+    let mainCellSrc = carouselCells.mainCell.src;
+    let rightCellSrc = carouselCells.rightCell.src;
+
+    debugger;
+    let indexOfLeftCell = pictureArray.indexOf(leftCellSrc);
+    let indexOfMainCell = pictureArray.indexOf(mainCellSrc);
+    let indexOfRightCell = pictureArray.indexOf(rightCellSrc);
+
+    if(indexOfLeftCell == pictureArray.length - 1) {
+        indexOfLeftCell = 0;
+        indexOfMainCell++;
+        indexOfRightCell++;
+    } else if(indexOfMainCell == pictureArray.length - 1) {
+        indexOfLeftCell++;
+        indexOfMainCell = 0;
+        indexOfRightCell++;
+    } else if(indexOfRightCell == pictureArray.length - 1) {
+        indexOfLeftCell++;
+        indexOfMainCell++;
+        indexOfRightCell = 0;
+    } else {
+        indexOfLeftCell++;
+        indexOfMainCell++;
+        indexOfRightCell++;
+    }
+
+    carouselCells.leftCell.src = pictureArray[indexOfLeftCell];
+    carouselCells.mainCell.src = pictureArray[indexOfMainCell];
+    carouselCells.rightCell.src = pictureArray[indexOfRightCell];
+});
+
+function nextPicture() {
+    
+}
+
+
 let pictureArray = [
-    'pics\\Beast_Wellington_FLATCROP.jpg',
-    'pics\\DR_Optmic_FLATCROP.jpg',
-    'pics\\Killzentine_FLATCROP.jpg',
-    'pics\\Knifey_FLATCROP.jpg',
-    'pics\\Mr_Mean_FLATCROP.jpg',
-    'pics\\One_Shoegore_FLATCROP.jpg',
-    'pics\\Rex_Bamboo_FLAT.jpg',
-    'pics\\S_Word_FLATCROP.jpg'
+    'file:///D:/Side%20projects/VirtualZoos/pics/Beast_Wellington_FLATCROP.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/DR_Optmic_FLATCROP.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/Killzentine_FLATCROP.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/Knifey_FLATCROP.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/Mr_Mean_FLATCROP.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/One_Shoegore_FLATCROP.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/Rex_Bamboo_FLAT.jpg',
+    'file:///D:/Side%20projects/VirtualZoos/pics/S_Word_FLATCROP.jpg'
 ];
 
+
+
 (function () {
-    debugger;
     let date = new Date();
     let year = date.getFullYear();
 
@@ -38,7 +110,7 @@ let pictureArray = [
 window.onload = function() {
     carouselCells.leftCell.src = pictureArray[0];
     carouselCells.mainCell.src = pictureArray[1];
-    carouselCells.rightCell.src = pictureArray[3];
+    carouselCells.rightCell.src = pictureArray[2];
 }
 
 window.onscroll = function() {
