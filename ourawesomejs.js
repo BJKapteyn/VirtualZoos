7c59d92a64d33b4bb81c3f893e7043f0e40c94ea
Brad <bjkapteyn@gmail.com>
2020-04-08 23:18:08 -0400

    added more pictures and started creating carousel

--- ourawesomejs.js
+++ ourawesomejs.js
@@ -1,4 +1,30 @@
-(function() {
+let navBar = document.getElementById('nav');
+let navBarOffsetTop = navBar.offsetTop;
+
+let buttons = {
+    previousButton: document.getElementById('leftButton'),
+    nextButton: document.getElementById('rightButton')
+};
+
+let carouselCells = {
+    leftCell: document.getElementById('leftCell'),
+    mainCell: document.getElementById('mainCell'),
+    rightCell: document.getElementById('rightCell')
+};
+
+let pictureArray = [
+    'pics\\Beast_Wellington_FLATCROP.jpg',
+    'pics\\DR_Optmic_FLATCROP.jpg',
+    'pics\\Killzentine_FLATCROP.jpg',
+    'pics\\Knifey_FLATCROP.jpg',
+    'pics\\Mr_Mean_FLATCROP.jpg',
+    'pics\\One_Shoegore_FLATCROP.jpg',
+    'pics\\Rex_Bamboo_FLAT.jpg',
+    'pics\\S_Word_FLATCROP.jpg'
+];
+
+(function () {
+    debugger;
     let date = new Date();
     let year = date.getFullYear();
 
@@ -9,25 +35,21 @@
     console.log("hey there");
 })();
 
+window.onload = function() {
+    carouselCells.leftCell.src = pictureArray[0];
+    carouselCells.mainCell.src = pictureArray[1];
+    carouselCells.rightCell.src = pictureArray[3];
+}
+
 window.onscroll = function() {
     this.sticky();
 }
-let youDidIt = document.getElementById('youDidIt');
-let navBar = document.getElementById('nav');
-let navBarOffsetTop = navBar.offsetTop;
 
 function sticky() {
     if(window.pageYOffset >= navBarOffsetTop){
-        youDidIt.style.display = "block";
         navBar.classList.add("navStick");
     } else {
         navBar.classList.remove("navStick");
     }
     
 };
-
-
-
-window.onscroll = function() {
-    this.sticky();
-}
